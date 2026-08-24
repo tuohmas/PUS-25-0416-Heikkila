7f78cc745ed0ce43d950f9a800a1739e363f172f
Tuomas Heikkilä <Localadmin_tkjheikk@LH2-700-26406.ad.helsinki.fi>
2026-08-24 11:33:30 +0300

    Cleaning code throughout

--- R/create_wordcloud.R
+++ R/create_wordcloud.R
@@ -1,10 +1,8 @@
 # Tuomas Heikkilä
 # tuomas.k.heikkila@helsinki.fi
 #
-#
 # Script for creating smart word clouds; Illustrate differences in vocabularies
 # Inspired by Johannes Gruber: https://www.johannesbgruber.eu/post/smarter-wordclouds/
-#
 #
 # PREPARATIONS #################################################################
 
@@ -17,45 +15,25 @@ if(!requireNamespace("pacman")) {
   library(pacman) }
 
 # Load packages
-pacman::p_load(dplyr,      # For data manipulation
-               tidyr,      # For data manipulation
-               purrr,      # For data manipulation
-               readr,      # For data manipulation
-               quanteda,
-               spacyr,
-               textstem,   # TEMP?
-               quanteda.textstats,  # textstat_frequency()
-               ggplot2,
-               ggwordcloud)
+pacman::p_load(dplyr,               # For data manipulation
+               tidyr,               # For data manipulation
+               purrr,               # For data manipulation
+               readr,               # For data manipulation
+               quanteda,            # For corpus linguistics
+               quanteda.textstats,  # For calculating keyness
+               ggplot2,             # For visualisations
+               ggwordcloud)         # For word cloud plots
 
 sessionInfo()
 
-# Load data
+# Load data (original data available upon request)
 labeled_tweets <- readRDS("data/clean_labeled_tweets.rds")
-# labeled_replies <- readRDS("data/clean_labeled_replies.rds")
 
-  # read_csv("2 REPLIES (social correction) - codes_correction.csv") %>%
-  # filter(!grepl("del|discard", flag, ignore.case = T))
+# Anonymised quanteda DFM subsetted to adjectives available on results folder
+dfm_adjectives <- readRDS("results/dfm_adjectives.rds")
+docnames(dfm_adjectives)
 
-# BRANCH DEL
-
-# ggplot(labeled_replies, aes(label = term, size = freq, color = term)) +
-#   geom_text_wordcloud() +
-#   scale_size_area(max_size = 15, trans = power_trans(1/.5)) +
-#   theme_minimal()
-#
-# library(ggwordcloud)
-#
-# # Data
-# df <- thankyou_words_small
-#
-# set.seed(1)
-# ggplot(df, aes(label = word, size = speakers, color = name)) +
-#   geom_text_wordcloud() +
-#   scale_size_area(max_size = 20) +
-#   theme_minimal()
-
-# / BRANCH
+# Skip to WORDCLOUD section when using anonymised data
 
 # TEXT PREPROCESS ##############################################################
 
@@ -63,27 +41,25 @@ labeled_tweets <- readRDS("data/clean_labeled_tweets.rds")
 # functionalities for removing stopwords, trimming corpus, and collocating
 # compound words
 
-# data <- labeled_tweets
-
-text_cleanup <- function(data,  # Exported and annotated df with text and doc id
-                         remove_stopwords = FALSE,
-                         stop_words = stopwords::stopwords("en"),
-                         word_collocations = FALSE,
-                         collocations_min_count = 3,
-                         collocations_p = 0.001,
-                         dfm_trim = FALSE,
-                         dfm_tfidf = FALSE) {
+text_cleanup <- function(
+    data,  # Exported and annotated df with text and doc id
+    remove_stopwords = FALSE,
+    stop_words = stopwords::stopwords("en"),
+    word_collocations = FALSE,
+    collocations_min_count = 3,
+    collocations_p = 0.001,
+    dfm_trim = FALSE,
+    dfm_tfidf = FALSE
+    ) {
 
   # Reduce and filter data frame
   keep_cols <- c("tweet_id", "text", "label", "code")
 
   data <- data %>%
-    # filter(check == "labeled") %>%
     select(all_of(keep_cols))
 
   # Clean, tokenize and pre-process
   tokens <- data %>%
-  # tc <- data %>%
 
     # Clean Twitter specific expressions and badly encoded characters
     mutate(text = gsub("[AT MULTIPLE USERS]", "", text, fixed = TRUE)) %>%
@@ -95,7 +71,6 @@ text_cleanup <- function(data,  # Exported and annotated df with text and doc id
     mutate(text = gsub("&gt;", "<", text)) %>%   # Greater than
     mutate(text = gsub("’", "'", text)) %>%   # Greater than
     mutate(text = gsub("=", " = ", text)) %>%   # Equal than
-
     mutate(text = gsub("\\?|\\)|\\(", "", text)) %>%
     mutate(text = tolower(text)) %>%
     mutate(text = gsub(
@@ -104,25 +79,8 @@ text_cleanup <- function(data,  # Exported and annotated df with text and doc id
     # Separate words inside hashtags ("#IvermectinSavesLives")
     mutate(text = gsub("((?<=[a-z]|[1-9])[A-Z])", " \\1", text, perl = TRUE)) %>%
 
-    # TEMP TEMP TEMP
-
-    # Create corpus
-    # corpustools::udpipe_tcorpus(
-    #   x = .$text,
-    #   model = "english-ewt",
-    #   doc_id = .$tweet_id)
-
-  # tc = create_tcorpus(sotu_texts[c(1:5,801:805),], doc_col='id')
-
-  # tcs1 = subset(tc, token_id < 20)
-
-  # tc_adjectives = subset(tc, POS == 'ADJ')
-
-  # dfm = corpustools::get_dfm(tc_adjectives, "lemma")
-
-    # Create corpus
-    corpus(docid_field = "tweet_id", text_field = "text") %>%
-
+    # Create corpus with quanteda package
+    quanteda::corpus(docid_field = "tweet_id", text_field = "text") %>%
 
     # Remove punctuation, symbols, and numbers; retain padding for later
     quanteda::tokens(what = "word",
@@ -130,9 +88,6 @@ text_cleanup <- function(data,  # Exported and annotated df with text and doc id
                      remove_symbols = TRUE,
                      remove_numbers = TRUE,
                      padding = word_collocations)
-
-    # # Lowercase
-    # tokens_tolower()
 
   # Optional: Remove stop words; reserve padding for later
   if(remove_stopwords) {
@@ -153,7 +108,7 @@ text_cleanup <- function(data,  # Exported and annotated df with text and doc id
                           min_count = collocations_min_count, # abs. threshold
                           size = 2) %>%       # bigrams only
 
-    # Keep collocations above critical value for 99.9% confidence level
+    # Keep collocations above critical value (defaults to 99% confidence level)
     filter(z > qnorm(p = collocations_p / 2, lower.tail = FALSE) |
            z < qnorm(p = collocations_p / 2)) %>%
 
@@ -206,21 +161,11 @@ text_cleanup <- function(data,  # Exported and annotated df with text and doc id
 
 }
 
-# Remove
+# Define custom stop words: include verbatim quotes of studies and their names
 study_data <- read_csv("data/retracted_studies.csv")
-
-included_studies <- read_lines("data/included_studies.txt") %>%
-  gsub("10.21203/rs\\.3\\.rs-100956/.*", "10.21203/rs.3.rs-10095", .) %>%
-  unique()
-
-# discard_doi = c("10.1016/S0140-6736(20)31180-6",
-#                 "10.21203/rs.3.rs-109670/v1",
-#                 "10.4269/ajtmh.20-0873")
 
 # Add when was the tweet posted relative to the study's publication date
 study_names <- study_data %>%
-  # filter(!doi %in% discard_doi)) %>%
-  filter(doi %in% included_studies) %>%
   select(title) %>%
   mutate(title = gsub("retracted: |retracted article: ", "", title,
                       ignore.case = TRUE)) %>%
@@ -251,7 +196,7 @@ clean_study_names <- clean_study_names %>%
   select(phrase) %>%
   filter(!is.na(phrase))
 
-glimpse(clean_study_names)
+clean_study_names
 
 journal_names <- data.frame(
   phrase = tolower(
@@ -286,33 +231,6 @@ clean_study_names <- clean_study_names %>%
   rbind(verbatim_quotes) %>%
   distinct(phrase)
 
-# TEMP DISCARD
-# test_tweet <- labeled_tweets %>% filter(tweet_id == "1435759330368835587")
-# test_tweet <- labeled_tweets %>% filter(tweet_id == "1412763555020128266")
-# test_tweet <- labeled_tweets %>% filter(tweet_id == "1429759785268883461")
-# test_tweet <- labeled_tweets %>% filter(tweet_id == "1395782723076337673")
-# test_tweet <- labeled_tweets %>% filter(tweet_id == "1413420493240217604")
-# test_tweet <- labeled_tweets %>% filter(tweet_id == "1427712948584206342")
-#
-# test_tweet %>%
-#
-# # Clean Twitter specific expressions and badly encoded characters
-# mutate(text = gsub("[AT MULTIPLE USERS]", "", text, fixed = TRUE)) %>%
-#   mutate(text = gsub("[LINK]", "", text, fixed = TRUE)) %>%
-#   mutate(text = gsub("#", "", text)) %>%
-#   mutate(text = gsub("@.+?\\b|@.+?\\.|@.+?,", "", text)) %>%
-#   mutate(text = gsub("&lt;", ">", text)) %>%   # Less than
-#   mutate(text = gsub("&gt;", "<", text)) %>%   # Greater than
-#   mutate(text = gsub("&gt;", "<", text)) %>%   # Greater than
-#   mutate(text = gsub("’", "'", text)) %>%   # Greater than
-#
-#   mutate(text = gsub("\\?|\\)|\\(", "", text)) %>%
-#   mutate(text = tolower(text)) %>%
-#   mutate(text = gsub(
-#     paste(clean_study_names$title, collapse = "|"), "", text)) %>%
-#
-#   select(text)
-
 clean_study_names
 
 # Combine multiple stop words dictionaries
@@ -333,13 +251,6 @@ my_stopwords <-
 keep_stopwords = c("no", "not", "more", "most", "less", "why")
 
 my_stopwords <- my_stopwords[!my_stopwords %in% keep_stopwords]
-
-# TEMP: See whether there are any adjectives we should keep (None found)
-my_stopwords %>% data.frame(word = .) %>%
-  left_join(tidytext::parts_of_speech) %>%
-  filter(pos %in% "Adjective") %>%
-  pull(word) %>%
-  unique
 
 # Use functions
 dfm_tweets <- labeled_tweets %>%
@@ -375,9 +286,10 @@ dfm_adjectives <-
 
 docnames(dfm_adjectives)
 
+# Join label data with tweet id as a key
 data.frame(docs = docnames(dfm_adjectives)) %>%
   left_join(labeled_tweets[, c("tweet_id", "code")],
-            by = c("docs" = "tweet_id")) %>%
+            by = c("docs" = "tweet_id"))  %>%
   pull(code)
 
 dfm_adjectives$code <-
@@ -396,9 +308,11 @@ dfm_adjectives
 topfeatures(dfm_adjectives, n = 200)
 textstat_frequency(dfm_adjectives, n = 50, groups = code, ties_method = "max")
 
-# dfm_replies
-# topfeatures(dfm_replies, n = 100)
-# textstat_frequency(dfm_replies, n = 50, groups = code, ties_method = "max")
+# Anonymise tweet ids from the dfm and save
+docnames(dfm_adjectives) <- paste("Tweet", seq_along(docnames(dfm_adjectives)))
+
+# Uncomment
+dfm_adjectives %>% saveRDS("results/dfm_adjectives.rds")
 
 # WORDCLOUD ####################################################################
 
@@ -433,13 +347,13 @@ calculate_keyness <- function(dfm,
   keyness_over <- keyness %>%
     mutate(
       frequency = (n_target + n_reference),
-      relfreq_target =  (n_target + 0.1) / (frequency + 0.1), # xlims: -2, 2 (log10)
-      relfreq_reference = (n_reference + 0.1) / (frequency + 0.1), # xlims: -2, 2 (log10)
+      relfreq_target =  (n_target + 0.1) / (frequency + 0.1),
+      relfreq_reference = (n_reference + 0.1) / (frequency + 0.1),
       overrepresentation = log10((relfreq_target) / (relfreq_reference)))
 
   # Add a new column for words pasted with significance level indicators
   plot_data <- keyness_over %>%
-    # slice_max(frequency, n = 150, with_ties = FALSE) %>% # Top words up to N # Redundant
+
     # Encode significance level to the feature displayed
     mutate(feature_sig = case_when(
       p < 0.001 ~ paste0(feature, "***"),
@@ -447,167 +361,80 @@ calculate_keyness <- function(dfm,
       p < 0.05 ~ paste0(feature, "*"),
       .default = feature), .after = feature) %>%
 
-    # Add boolean variable to indicate significance (to highlight w/ disc. alpha)
+    # Add boolean variable to highlight statistically significant terms
     mutate(significant = ifelse(p < 0.05, TRUE, FALSE))
 
   return(plot_data)
 
 }
 
-# Apply to tweets
-# keyness_tweets <- dfm_tweets %>%
-#   calculate_keyness(target_code = "P", # Compare negative to positive (reference)
-#                     # subset_by = c("A", "U"), # Remove Neutral and Unclassified
-#                     textstat_frequency = 200)
-
 keyness_tweets <- dfm_adjectives %>%
-  calculate_keyness(target_code = "P", # Compare negative to positive (reference)
-                    # subset_by = c("A", "U"), # Remove Neutral and Unclassified
-                    textstat_frequency = 200)
+  calculate_keyness(target_code = "P", # Compare positive to the rest
+                    textstat_frequency = 200) # choose 200 most common terms
 
 keyness_tweets <- keyness_tweets %>%
   arrange(p) %>%
-  slice_max(frequency, n = 200, with_ties = FALSE)
+  slice_max(frequency, n = 200, with_ties = FALSE) # up to 200 most common terms
 
 glimpse(keyness_tweets)
 
 keyness_tweets %>% slice_min(p, n = 10)
 
-# keyness_tweets %>%
-#   arrange(desc(chi2)) %>%
-#   head(n = 50)
-#
-# keyness_tweets %>%
-#   arrange(chi2) %>%
-#   head(n = 50)
+# Custom size breaks for the word cloud
+size_breaks <- c(min(keyness_tweets$frequency),
+                 seq(from = 25, to = max(keyness_tweets$frequency), by = 25),
+                 max(keyness_tweets$frequency))
 
-# Apply to relies
-# keyness_replies <- dfm_replies %>%
-#   calculate_keyness(target_code = "1",
-#                     textstat_frequency = 100) %>%
-#   # arrange(desc(relfreq_target))
-#   arrange(desc(relfreq_reference))
+# Plot wordcloud
+wordplot <- keyness_tweets %>%
+  ggplot(aes(
+    x = overrepresentation,
+    y = chi2,
+    label = feature_sig,
+    size = frequency,
+    colour = overrepresentation)) +
 
-# head(keyness_replies, n = 100)
+  geom_text_wordcloud(
+    aes(alpha = 1 - p),
+    grid_margin = 0.75, # default 1
+    seed = 1234,
+    eccentricity = 0.75,
+    max_steps = 5,
+    shape = "diamond", # default "circle"
+    show.legend = TRUE) +
 
-# plot_data <- keyness_tweets
+  scale_size_area(
+    max_size = 10,
+    breaks = size_breaks) +
 
-# PLOT AS A FUNCTION
-wordcloud_plot <- function(plot_data) {
-
-  # TEMP
-  # size_breaks <- c(min(plot_data$frequency), 50,
-  #                  seq(from = 100, to = max(plot_data$frequency), by = 100),
-  #                  max(plot_data$frequency))
-
-  size_breaks <- c(min(plot_data$frequency),
-                   seq(from = 25, to = max(plot_data$frequency), by = 25),
-                   max(plot_data$frequency))
+  scale_colour_gradient(low =  "red", high = "blue") +
 
 
-  # Plot
-  wordplot <- plot_data %>%
-    ggplot(aes(
-      x = overrepresentation,
-      y = chi2,
-      label = feature_sig,
-      size = frequency,
-      colour = overrepresentation)) +
+  # Group 1: right-pointing arrow and label
+  annotate("segment", x = 0.3, xend = 1.15, y = -18, colour = "black",
+           lineend = "butt", linejoin = "round", linewidth = 0.5,
+           arrow = arrow(length = unit(4, "pt"), type = "closed")) +
 
-    # Plot word cloud
-    geom_text_wordcloud(
-      # aes(alpha = significant), # TEMP
-      aes(alpha = 1 - p),
-                        grid_margin = 0.75, # default 1
-                        seed = 1234,
-                        eccentricity = 0.75,
-                        max_steps = 5,
-                        shape = "diamond", # default "circle"
-                        show.legend = TRUE) +
+  annotate("text", x = 1.2, y = -18, size = 3, hjust = "left",
+           label = "Words more common in \n positive-stance tweets (blue)") +
 
-    # TEMP
-    # "Highlight" statistically significant values of chi2 (alpha = 0.05)
-    # scale_alpha_discrete(range = c(0.5, 1)) +
+  # Group 2: left-pointing arrow and label
+  annotate("segment", x = -0.3, xend = -1.15, y = -18, colour = "black",
+           lineend = "butt", linejoin = "round", linewidth = 0.5,
+           arrow = arrow(length = unit(4, "pt"), type = "closed")) +
 
-    scale_size_area(
-      max_size = 10,
-      breaks = size_breaks) +
+  annotate("text", x = -1.2, y = -18, size = 3, hjust = "right",
+           label = "Words more common \nin rest of the tweets (red)") +
 
-    scale_colour_gradient(low =  "red", high = "blue") +
+  theme_minimal() +
 
+  labs(y = expression("Keyness (" ~ chi^{2} * ")"),
+       x = expression("Overrepresentation (" * log[10] * ")")) +
 
-    # Group 1: right-pointing arrow and label
-    annotate("segment", x = 0.3, xend = 1.15, y = -18, colour = "black",
-             lineend = "butt", linejoin = "round", linewidth = 0.5,
-             arrow = arrow(length = unit(4, "pt"), type = "closed")) +
+  guides(colour = "none", size = "legend", Overrepresentation = "none",
+         alpha = "none") +
 
-    annotate("text", x = 1.2, y = -18, size = 3, hjust = "left",
-             label = "Words more common in \n positive-stance tweets (blue)") +
-
-    # Group 2: left-pointing arrow and label
-    annotate("segment", x = -0.3, xend = -1.15, y = -18, colour = "black",
-             lineend = "butt", linejoin = "round", linewidth = 0.5,
-             arrow = arrow(length = unit(4, "pt"), type = "closed")) +
-
-    annotate("text", x = -1.2, y = -18, size = 3, hjust = "right",
-             label = "Words more common \nin rest of the tweets (red)") +
-
-    theme_minimal() +
-
-    labs(y = expression("Keyness (" ~ chi^{2} * ")"),
-         x = expression("Overrepresentation (" * log[10] * ")")) +
-
-    guides(colour = "none", size = "legend", Overrepresentation = "none",
-           alpha = "none"
-           # alpha = guide_legend("p-value") # Not working, wrong order
-           ) +
-
-    coord_cartesian(xlim = c(-3, 3), ylim = c(-20, 20))
-
-  return(wordplot)
-
-}
-
-# set.seed(4321)
-
-# data("love_words_latin_small")
-#
-# ggplot(love_words_latin_small, aes(label = word, size = speakers)) +
-#   geom_text_wordcloud(eccentricity = 0.75, max_steps = 5) +
-#   scale_size_area(max_size = 20) +
-#   theme_minimal()
-
-# Optional: Trim outliers
-# keyness_tweets <- keyness_tweets %>% filter(between(chi2, -100, 100))
-# keyness_tweets <- keyness_tweets %>% filter(frequency > 2)
-# keyness_tweets <- keyness_tweets %>% filter(frequency < 300)
-
-# keyness_tweets %>% nrow()
-
-# Plot data
-wordcloud <- wordcloud_plot(keyness_tweets)
-
-wordcloud
-
-wordcloud$data
-
-discard_data <- c("feature", "n_target", "n_reference",
-                  "relfreq_target", "relfreq_reference", "significant")
-
-figure_raw_data <-  wordcloud$data %>%
-  select(-all_of(discard_data)) %>%
-  rename("feature" = "feature_sig") %>%
-  write_excel_csv("data/figure_raw_data.csv")
-
-figure_raw_data
-
-figure_plotted_data <- wordcloud$data %>%
-  rename("y" = "chi2",
-         "size" = "frequency",
-         "x" = "overrepresentation") %>%
-  mutate(alpha = 1 - p) %>%
-  select(feature, size, x, y, alpha) %>%
-  write_excel_csv("data/figure_plotted_data.csv")
+  coord_cartesian(xlim = c(-3, 3), ylim = c(-20, 20))
 
 # Save plotted files
 
@@ -643,5 +470,3 @@ ggsave(
   units = "mm",
   dpi = 800
 )
-
-# FINISHED
